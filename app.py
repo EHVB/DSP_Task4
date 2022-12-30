d2c6aa62812d9c4796e6a418c0e74b7972c5c4b9
EHVB <ehab.mahmoud00@eng-st.cu.edu.eg>
2022-12-30 23:49:35 +0200

    extracting phase and magnitude function

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, request
 from PIL import Image
 from flask import send_file
+import numpy as np
+import matplotlib.pyplot as plt
 app = Flask(__name__)
 from werkzeug.utils import secure_filename
 import os
 import cv2
+
+def extract_info(filename,filepath):
+    img = cv2.imread(filepath,0)
+    img_fft = np.fft.fftshift(np.fft.fft2(img))
+    img_amplitude = np.sqrt(np.real(img_fft) ** 2 + np.imag(img_fft) ** 2)
+    img_phase = np.arctan2(np.imag(img_fft), np.real(img_fft))
+    plt.imsave(f"static/images/{filename}_mag.jpg",np.log(img_amplitude+1e-10), cmap='gray') 
+    plt.imsave(f"static/images/{filename}_phase.jpg",img_phase, cmap='gray')
+    return f"static/images/{filename}_mag.jpg","static/images/{filename}_phase.jpg"
 
 
 @app.route('/', methods=['GET','POST'])
@@ -14,11 +25,8 @@ def home():
         filename = secure_filename(image1.filename) # save file 
         filepath = os.path.join('static/images', filename);
         image1.save(filepath)
-        cv2.imread(filepath)
-        # do processing here 
-        # /////
-        magpath=r"static\images\img2.jpg"  # should be replaced by your path for mag
-        return (magpath)
+        mag_path,phase_path = extract_info(filename,filepath)
+        return (mag_path)
     else:
         return (render_template('index.html'))
 
