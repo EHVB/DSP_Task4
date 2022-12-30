7fe4f0b32728c1ec2b30400da23db91e596f1daa
ayasameh1 <aya.ahmed012@eng-st.cu.edu.eg>
2022-12-30 20:55:30 +0200

    buttons updated

--- processing/upload/up.js
+++ processing/upload/up.js
@@ -22,17 +22,17 @@ $(".file_remove").on("click", function (e) {
 	btnOuter.removeClass("file_uploading");
 	btnOuter.removeClass("file_uploaded");
 });
-var btnUpload = $("#upload_file2"),
-	btnOuter = $(".button_outer2");
-btnUpload.on("change", function (c) {
-	var ext = btnUpload.val().split('.').pop().toLowerCase();
-	if ($.inArray(ext, ['gif', 'png', 'jpg', 'jpeg']) == 0) {
+var btnUpload2 = $("#upload_file2"),
+	btnOuter2 = $(".button_outer2");
+btnUpload2.on("change", function (c) {
+	var ext2 = btnUpload2.val().split('.').pop().toLowerCase();
+	if ($.inArray(ext2, ['gif', 'png', 'jpg', 'jpeg']) == 0) {
 		$(".error_msg").text("Not an Image...");
 	} else {
 		$(".error_msg").text("");
-		btnOuter.addClass("file_uploading2");
+		btnOuter2.addClass("file_uploading2");
 		setTimeout(function () {
-			btnOuter.addClass("file_uploaded2");
+			btnOuter2.addClass("file_uploaded2");
 		}, 6000);
 		var uploadedFile2 = URL.createObjectURL(c.target.files[0]);
 		setTimeout(function () {
@@ -44,6 +44,6 @@ $(".file_remove2").on("click", function (c) {
 
 	$("#uploaded_view2").removeClass("show");
 	$("#uploaded_view2").find("img").remove();
-	btnOuter.removeClass("file_uploading2");
-	btnOuter.removeClass("file_uploaded2");
+	btnOuter2.removeClass("file_uploading2");
+	btnOuter2.removeClass("file_uploaded2");
 });
