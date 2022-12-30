825d7ffa84d744b10f86d1e690ac1cafa318e06c
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2022-12-30 22:53:16 +0200

    send image to backend

--- app.py
+++ app.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template, request
 from PIL import Image
 from flask import send_file
 app = Flask(__name__)
+from werkzeug.utils import secure_filename
+import os
+import cv2
 
-@app.route('/')
+
+@app.route('/', methods=['GET','POST'])
 def home():
-    return (render_template('index.html'))
+    if request.method=='POST' and request.form['requestinfo']== 'image1':
+        image1 = request.files['image1']
+        filename = secure_filename(image1.filename) # save file 
+        filepath = os.path.join('static/images', filename);
+        image1.save(filepath)
+        cv2.imread(filepath)
+        # do processing here 
+        # /////
+        magpath=r"static\images\img2.jpg"  # should be replaced by your path for mag
+        return (magpath)
+    else:
+        return (render_template('index.html'))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -3,14 +3,34 @@ var loadFile1 = function(event) {
 	console.log(event.target.files.length);
  	var image = document.getElementById('img1');
 	image.src = URL.createObjectURL(event.target.files[0]);
+	var xhr=new XMLHttpRequest();
+    var fd=new FormData();
+	fd.append("image1",event.target.files[0],event.target.files[0].filename);
+	fd.append("requestinfo","image1" );
+	xhr.open("POST","/",true);
+	xhr.send(fd);
+	xhr.onload=function(e) {
+		if(this.readyState === 4) {
+			console.log("Server returned: ",e.target.responseText);
+			var image4 = document.getElementById('img4');
+			console.log(e.target.responseText.length);
+			response=e.target.responseText;		  
+			image4.src=response;
+	
 
-    
-};
+
+		}
+	};
+
+	};
 
 var loadFile2 = function(event) {
 	console.log(event.target.files.length);
  	var image = document.getElementById('img2');
+
 	image.src = URL.createObjectURL(event.target.files[0]);
+
+
 
     
 };
