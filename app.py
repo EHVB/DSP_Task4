815cd2a7b4f051ec720770f899bcd8f0dd667870
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2022-12-31 12:04:54 +0200

    phase in img2

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ def extract_info(filename,filepath):
     img_phase = np.arctan2(np.imag(img_fft), np.real(img_fft))
     plt.imsave(f"static/images/{filename}_mag.jpg",np.log(img_amplitude+1e-10), cmap='gray') 
     plt.imsave(f"static/images/{filename}_phase.jpg",img_phase, cmap='gray')
-    return f"static/images/{filename}_mag.jpg","static/images/{filename}_phase.jpg"
+    magpath=(f"static/images/{filename}_mag.jpg")
+    phasepath=(f"static/images/{filename}_phase.jpg")
+    return magpath,phasepath
 
 
 @app.route('/', methods=['GET','POST'])
@@ -27,6 +29,14 @@ def home():
         image1.save(filepath)
         mag_path,phase_path = extract_info(filename,filepath)
         return (mag_path)
+    elif request.method=='POST' and request.form['requestinfo']== 'image2':
+        image2 = request.files['image2']
+        filename = secure_filename(image2.filename) # save file 
+        filepath = os.path.join('static/images', filename);
+        image2.save(filepath)
+        mag_path,phase_path = extract_info(filename,filepath)
+        return (phase_path) 
+
     else:
         return (render_template('index.html'))
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
 
+let crop1;
+let crop2; 
 
 var loadFile1 = function(event) {
 	console.log(event.target.files.length);
  	var image = document.getElementById('img1');
-	var image2 = document.getElementById('imgtry');
-	var result = document.getElementById('result');
 	image.src = URL.createObjectURL(event.target.files[0]);
 	var image4 = document.getElementById('img4');
 	crop1= Jcrop.attach(image4);	
@@ -32,8 +32,30 @@ var loadFile1 = function(event) {
 var loadFile2 = function(event) {
 	console.log(event.target.files.length);
  	var image = document.getElementById('img2');
-
 	image.src = URL.createObjectURL(event.target.files[0]);
+	var image3 = document.getElementById('img3');
+	console.log(event.target.id)
+	crop2= Jcrop.attach(image3);	
+	var xhr=new XMLHttpRequest();
+    var fd=new FormData();
+	fd.append("image2",event.target.files[0],event.target.files[0].filename);
+	fd.append("requestinfo","image2" );
+	xhr.open("POST","/",true);
+	xhr.send(fd);
+	xhr.onload=function(e) {
+		if(this.readyState === 4) {
+			console.log("Server returned: ",e.target.responseText);
+			var image3 = document.getElementById('img3');
+			console.log(e.target.responseText.length);
+			response=e.target.responseText;		  
+			image3.src=response;
+	
+
+
+		}
+	};
+
+
 };
 	
 
