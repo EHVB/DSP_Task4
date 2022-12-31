994d596e9da29d246cac22eea00cfd4d18b5c4e2
ayasameh1 <aya.ahmed012@eng-st.cu.edu.eg>
2022-12-31 19:09:23 +0200

    functions class added

---
+++ class.py
@@ -0,0 +1,34 @@
+class Functions:
+    def getfft( filepath):
+      img = cv2.imread(filepath, 0)
+      img_fft = np.fft.fftshift(np.fft.fft2(img))
+      return img_fft
+
+
+    def getmag(filename,img_fft):
+        img_amplitude = np.sqrt(np.real(img_fft)** 2 + np.imag(img_fft) ** 2)
+        magpath = (f"static/images/{filename}_mag.jpg")
+        plt.imsave(f"static/images/{magpath}_mag.jpg",
+                   np.log(img_amplitude+1e-10), cmap='gray')
+        return magpath
+
+    def getphase(filename,img_fft):
+        img_phase = np.arctan2(np.imag(img_fft), np.real(img_fft))
+        phasepath = (f"static/images/{filename}_phase.jpg")
+        plt.imsave(f"static/images/{phasepath}_phase.jpg", img_phase, cmap='gray')
+        return phasepath
+
+
+
+
+
+    # def extract_info(filename,filepath):
+    # img = cv2.imread(filepath,0)
+    # img_fft = np.fft.fftshift(np.fft.fft2(img))
+    # img_amplitude = np.sqrt(np.real(img_fft) ** 2 + np.imag(img_fft) ** 2)
+    # img_phase = np.arctan2(np.imag(img_fft), np.real(img_fft))
+    # plt.imsave(f"static/images/{filename}_mag.jpg",np.log(img_amplitude+1e-10), cmap='gray') 
+    # plt.imsave(f"static/images/{filename}_phase.jpg",img_phase, cmap='gray')
+    # magpath=(f"static/images/{filename}_mag.jpg")
+    # phasepath=(f"static/images/{filename}_phase.jpg")
+    # return magpath,phasepath
